07aa04e012d3335d7146835f2102a4b34a32ca50
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-04-06 09:12:59 +0200

    add svg background. App needs to refactor small mobile layout

--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -12,7 +12,8 @@ const GlobalStyle = createGlobalStyle`
 
     html{
         color: #333;
-        background-color: #ddd;
+        background-color: #0d0d0d;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 100 100'%3E%3Crect x='0' y='0' width='78' height='78' fill-opacity='0.54' fill='%23292626'/%3E%3C/svg%3E");
     }
 
     body{
