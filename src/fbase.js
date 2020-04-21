9afadab06b8818df5db91280657c766a56cf461e
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-04-21 22:17:39 +0200

    install firebase, create config file and hide the one with key

---
+++ src/fbase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import { API_FIREBASE } from './protected';
+
+const firebaseConfig = {
+  apiKey: API_FIREBASE,
+  authDomain: 'pantry-29c74.firebaseapp.com',
+  databaseURL: 'https://pantry-29c74.firebaseio.com',
+  projectId: 'pantry-29c74',
+  storageBucket: 'pantry-29c74.appspot.com',
+  messagingSenderId: '31589261280',
+  appId: '1:31589261280:web:509bf9b9f45520ea151585',
+  measurementId: 'G-MM5HJRBGB0',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore;
+
+export default db;
