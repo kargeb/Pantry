6e315c98bc12f2f95169696589cfece7d4cc51bd
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-01-03 09:29:05 +0100

    add auth listener and save currentuser in state

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { auth } from '../fbase';
 
-class App extends Component {
+class App extends React.Component {
   state = {
-    currentUser: '',
+    currentUserId: '',
   };
 
+  componentDidMount() {
+    this.unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        console.log('ZALOGOWANY z LISTENERA w LOGIN:', user);
+        this.setState({ currentUserId: user.uid });
+      } else {
+        console.log('NIE ZALOGOWANY z LISTENERA w LOGIN:', user);
+        this.setState({ currentUserId: user.uid });
+      }
+    });
+  }
+
   render() {
+    const { currentUserId } = this.state;
+
     return (
       <div>
         <h1>Im in the new Root</h1>
+        <p>Current User: {currentUserId}</p>
+        {currentUserId ? (
+          <p>JEST TO BO JEST UZYTKOWNIK {currentUserId}</p>
+        ) : (
+          <p>NIE MA TEGO BO NIE MA UZYTKOWNIKA {currentUserId} </p>
+        )}
       </div>
     );
   }
