87dd884de54bf780daca285d6c6ae047d606671f
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-01-22 21:20:30 +0100

    move nac to shared

--- src/components/shared/navigation/Navigation.jsx
+++ src/components/shared/navigation/Navigation.jsx
@@ -7,7 +7,8 @@ import {
   faShoppingCart,
 } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
-import { AppContext } from '../../context';
+// import { AppContext } from '../../context';
+import { AppContext } from '../../../context';
 
 const Header = styled.header`
   background-color: ${({ theme }) => theme.primary};
